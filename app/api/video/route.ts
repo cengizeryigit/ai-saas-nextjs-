ad56365e100f8495822bb58aaa1207de3a6ffd83
cengizeryigit <cengiz_eryigit@hotmail.com>
2025-05-02 15:40:59 +0300

    Video Generation AI

---
+++ app/api/video/route.ts
@@ -0,0 +1,39 @@
+import { auth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+import { Configuration, OpenAIApi } from "openai";
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN!,
+});
+
+export async function POST(req: Request) {
+  try {
+    const { userId } = auth();
+
+    const body = await req.json();
+    const { prompt } = body;
+
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!prompt) {
+      return new NextResponse("Message is required", { status: 400 });
+    }
+
+    const response = await replicate.run(
+      "riffusion/riffusion:video-generation",
+      {
+        input: {
+          prompt_a: prompt,
+        },
+      }
+    );
+
+    return NextResponse.json(response);
+  } catch (error) {
+    console.log("[VIDEO_ERROR]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
